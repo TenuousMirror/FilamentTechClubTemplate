3b8861a4913dd05c77577647d860fea4bacaa482
TenuousMirror <133063542+TenuousMirror@users.noreply.github.com>
2023-05-12 17:53:49 -0500

    button color change

--- script/script.js
+++ script/script.js
@@ -1,3 +1,24 @@
 function changeColor() {
-document.getElementById("muffins").style.color = "purple";
+var currentcolor = document.getElementById("muffins").style.color;
+var newcolor = selectNewColor (currentcolor);
+document.getElementById("muffins").style.color = newcolor;
+}
+
+
+function selectNewColor(currentcolor) {
+var newcolor = "";
+
+if (currentcolor == "") {
+newcolor = "purple";
+
+} else if (currentcolor == "purple") {
+newcolor = "green";
+
+} else if (currentcolor == "green") {
+    newcolor == "red";
+} else if (currentcolor == "red") {
+newcolor = "";
+}
+return newcolor;
+
 }
